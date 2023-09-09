a0022cfc5b31d82b3fc03403a70dff0025c0d11b
Gaurav-Dev24 <subhamkr1995dob@gmail.com>
2023-09-09 13:43:33 +0530

    HeroSection and Home Route added

--- react-website/src/components/HeroSection.js
+++ react-website/src/components/HeroSection.js
@@ -9,7 +9,7 @@ function HeroSection() {
   return (
     <div className='hero-container'>
         <video src="/videos/video-1.mp4" autoPlay loop muted />
-        <h1>ADEVENTURE AWAITS</h1>
+        <h1>ADVENTURE AWAITS</h1>
         <p>What are waiting for?</p>
         <div className="hero-btns">
             <Button className="btns" buttonStyle="btn--outline" buttonSize="btn--large">
